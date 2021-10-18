46f97466cf706bb68c7bb12b36216ba0ef5ccb83
Luiz <luiz.fss@hotmail.com>
2021-10-18 20:08:48 -0300

    Salvando o usuário logado

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:get_it/get_it.dart';
 import 'package:parse_server_sdk/parse_server_sdk.dart';
 import 'package:xlo_mobx_parse_server/screens/base/base_screen.dart';
 import 'package:xlo_mobx_parse_server/stores/page_store.dart';
+import 'package:xlo_mobx_parse_server/stores/user_manager_store.dart';
 
 
 void main() async{
@@ -20,9 +21,10 @@ void setupLocators(){
   * você utiliza ele para localizar os serviços do seu app
   * e o PageStore é um serviço*/
 
-  /*Sigleton significa que vai exitir só uma instancia
+  /*Sigleton significa que vai existir só uma instância
   * no app inteiro, então sempre que for chamado será sempre a mesma*/
   GetIt.I.registerSingleton(PageStore());
+  GetIt.I.registerSingleton(UserManagerStore());
   print("setup");
 }
 
@@ -39,19 +41,19 @@ Future<void> initializeParse()async{
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
       title: 'XLO',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
+      theme: new ThemeData(
         primarySwatch: Colors.purple,
         scaffoldBackgroundColor: Colors.purple,
-        appBarTheme: AppBarTheme(
+        appBarTheme: new AppBarTheme(
           elevation: 0
         ),
         cursorColor: Colors.orange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: BaseScreen(),
+      home: new BaseScreen(),
     );
   }
 }
